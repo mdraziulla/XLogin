616d4b34a84df17ecfb966048278ed35be215c95
Md Razi Ullah <mdraziullah631@gmail.com>
2024-03-27 21:15:54 +0530

    Full Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import LoginPage from './login'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <LoginPage />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/login.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+
+function LoginPage() {
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const [isSubmit, setIsSubmit] = useState(false);
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        if(userName=="Crio" && password=="password"){
+            setError("");
+            setIsSubmit(true);
+        }else{
+            setError("Invalid username or password");
+            setIsSubmit(false);
+        }
+    }
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {isSubmit ? (
+        <div>
+        <p>Welcome, {userName}!</p>
+      </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          {error && <p className="error">{error}</p>}
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              placeholder="username"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      )}
+    </div>
+  )
+}
+
+export default LoginPage
